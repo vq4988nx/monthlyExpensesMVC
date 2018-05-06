db54bc04cbd2a1ed3b48e0c1d6a48bf64f45aecf
vq4988nx <vq4988nx@go.minneapolis.edu>
2018-05-06 01:19:52 -0500

    Updated decimals, etc, and changed sorting a little

--- src/main/java/com/jeff/monthlyexpense/repository/ExpenseRepository.java
+++ src/main/java/com/jeff/monthlyexpense/repository/ExpenseRepository.java
@@ -3,11 +3,18 @@ package com.jeff.monthlyexpense.repository;
 import com.jeff.monthlyexpense.model.Expense;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ExpenseRepository extends PagingAndSortingRepository<Expense, Long> {
     
 
+//    Was getting an error saying For queries with named parameters you need to use provide
+//    names for method parameters. Use @Param for query method parameters
+//    https://www.petrikainulainen.net/programming/spring-framework/spring-data-jpa-tutorial-introduction-to-query-methods/
+
+
     @Query(value = "select sum(e.amount) from Expense e where e.category=:catName")
-    Double getTotalExpensesByCategory(String catName);
+    Double getTotalExpensesByCategory(@Param("catName") String catName);
+
     
 }
