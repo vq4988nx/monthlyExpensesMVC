f9f7bfabc8420f5e8a39be9ba5b0689e6db39fe3
vq4988nx <vq4988nx@go.minneapolis.edu>
2018-05-08 00:16:49 -0500

    Completed categories and put in sample data for all categories

--- src/main/java/com/jeff/monthlyexpense/controller/ExpenseController.java
+++ src/main/java/com/jeff/monthlyexpense/controller/ExpenseController.java
@@ -24,12 +24,35 @@ public class ExpenseController {
     @Autowired
     public ExpenseController(ExpenseRepository expenses) {
         this.expenses = expenses;
-        
-        expenses.save(new Expense(new Date(), 50, "Stuff", "Target"));
-        expenses.save(new Expense(new Date(), 101, "More Stuff", "Target"));
-        expenses.save(new Expense(new Date(), 22, "Even more Stuff", "Target"));
-        expenses.save(new Expense(new Date(), 22, "Even more Stuff", "Clothing"));
-        expenses.save(new Expense(new Date(), 150, "Collars and Leashes", "Pets"));
+
+        expenses.save(new Expense(new Date(), 6, "Panera", "Eating Out"));
+        expenses.save(new Expense(new Date(), 800, "Rent", "Mortgage/Rent"));
+        expenses.save(new Expense(new Date(), 70, "Verizon monthly", "Monthly Bills"));
+        expenses.save(new Expense(new Date(), 85, "Cub", "Groceries/Target"));
+        expenses.save(new Expense(new Date(), 50, "Twins Game", "Entertainment"));
+        expenses.save(new Expense(new Date(), 101, "Vet Appointment", "Pets"));
+        expenses.save(new Expense(new Date(), 33, "MnGasCo", "Monthly Bills"));
+        expenses.save(new Expense(new Date(), 62, "Water Bill", "Monthly Bills"));
+        expenses.save(new Expense(new Date(), 48, "XCel Electric", "Monthly Bills"));
+        expenses.save(new Expense(new Date(), 45, "Birthday Present", "Gifts"));
+        expenses.save(new Expense(new Date(), 160, "Discount Tires", "Transportation"));
+        expenses.save(new Expense(new Date(), 7, "Chipotle", "Eating Out"));
+        expenses.save(new Expense(new Date(), 32, "Walgreens Prescription", "Medical"));
+        expenses.save(new Expense(new Date(), 140, "Target", "Groceries/Target"));
+        expenses.save(new Expense(new Date(), 190, "MOA Spree", "Clothing"));
+        expenses.save(new Expense(new Date(), 35, "US Internet", "Monthly Bills"));
+        expenses.save(new Expense(new Date(), 6, "MCTC Cafeteria", "Eating Out"));
+        expenses.save(new Expense(new Date(), 32, "Bull Dog", "Entertainment"));
+        expenses.save(new Expense(new Date(), 120, "Hotel in Duluth", "Travel"));
+        expenses.save(new Expense(new Date(), 80, "Geico Auto Ins", "Monthly Bills"));
+        expenses.save(new Expense(new Date(), 400, "Joe's Garage - Alternator", "Transportation"));
+        expenses.save(new Expense(new Date(), 74, "Amazon stuff", "Amazon"));
+        expenses.save(new Expense(new Date(), 30, "SA Gas", "Transportation"));
+        expenses.save(new Expense(new Date(), 5, "Lunds", "Eating Out"));
+        expenses.save(new Expense(new Date(), 35, "Chuck and Dons", "Pets"));
+        expenses.save(new Expense(new Date(), 6, "Black Panther", "Entertainment"));
+        expenses.save(new Expense(new Date(), 35, "Concert", "Entertainment"));
+        expenses.save(new Expense(new Date(), 50, "Wedding Gift", "Gifts"));
     }
 
     //This will always direct to the home page
@@ -55,24 +78,51 @@ public class ExpenseController {
     //    new mapping, experiment
     @RequestMapping("/categoriesTable")
     public String categoriesTable(Model model) {
-        double totalExpensesTarget = expenses.getTotalExpensesByCategory("Target");
-        // if there's no Target expenses, this returns null, so check for null. Adding nulls to the model causes an exception.
-        System.out.println("total expenses at target " + totalExpensesTarget);
-        model.addAttribute("totalExpensesTarget", totalExpensesTarget);
+        double totalExpensesMortgageRent = expenses.getTotalExpensesByCategory("Mortgage/Rent");
+        // if there's no Mortgage/Rent expenses, this returns null, so check for null. Adding nulls to the model causes an exception.
+        model.addAttribute("totalExpensesMortgageRent", totalExpensesMortgageRent);
 
-        double totalExpensesClothing = expenses.getTotalExpensesByCategory("Clothing");
-        System.out.println("total expenses for clothing " + totalExpensesClothing);
-        model.addAttribute("totalExpensesClothing", totalExpensesClothing);
+        double totalExpensesMonthlyBills = expenses.getTotalExpensesByCategory("Monthly Bills");
+        model.addAttribute("totalExpensesMonthlyBills", totalExpensesMonthlyBills);
 
+        double totalExpensesEatingOut = expenses.getTotalExpensesByCategory("Eating Out");
+        model.addAttribute("totalExpensesEatingOut", totalExpensesEatingOut);
+
+        double totalExpensesGroceriesTarget = expenses.getTotalExpensesByCategory("Groceries/Target");
+        model.addAttribute("totalExpensesGroceriesTarget", totalExpensesGroceriesTarget);
+
+        double totalExpensesMedical = expenses.getTotalExpensesByCategory("Medical");
+        model.addAttribute("totalExpensesMedical", totalExpensesMedical);
 
         double totalExpensesPets = expenses.getTotalExpensesByCategory("Pets");
-        System.out.println("total expenses for pets " + totalExpensesPets);
         model.addAttribute("totalExpensesPets", totalExpensesPets);
 
+        double totalExpensesEntertainment = expenses.getTotalExpensesByCategory("Entertainment");
+        model.addAttribute("totalExpensesEntertainment", totalExpensesEntertainment);
 
+        double totalExpensesTravel = expenses.getTotalExpensesByCategory("Travel");
+        model.addAttribute("totalExpensesTravel", totalExpensesTravel);
+
+        double totalExpensesAmazon = expenses.getTotalExpensesByCategory("Amazon");
+        model.addAttribute("totalExpensesAmazon", totalExpensesAmazon);
+
+        double totalExpensesGifts = expenses.getTotalExpensesByCategory("Gifts");
+        model.addAttribute("totalExpensesGifts", totalExpensesGifts);
+
+        double totalExpensesClothing = expenses.getTotalExpensesByCategory("Clothing");
+        model.addAttribute("totalExpensesClothing", totalExpensesClothing);
 
         return "categoriesTable.html";
 //        return new ModelAndView("categoriesTable.html", "expense", new Expense());
+
+
+
+
+
+
+
+
+
     }
 
 
@@ -107,4 +157,7 @@ public class ExpenseController {
     }
 
 
+
 }
+
+
